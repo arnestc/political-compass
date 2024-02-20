de1a2bcbd46655ec7b9352899ac04aecd360b0f3
Federico Cinus <33600954+FedericoCinus@users.noreply.github.com>
2024-02-20 19:01:52 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-processed_data_path = "../../data/processed/"
-raw_data_path = "../../data/raw/"
-results_data_path = "../../data/results/"
-figure_path = "../../figures/"
+processed_data_path = "../data/processed/"
+raw_data_path = "../data/raw/"
+results_data_path = "../data/results/"
+figure_path = "../figures/"
